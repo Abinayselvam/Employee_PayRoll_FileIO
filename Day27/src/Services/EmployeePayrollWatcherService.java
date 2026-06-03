a79f2602434a84851dfa3a614a476c23c899ca62
AbinayaSelvam <sabinaya352@gmail.com>
2026-06-03 13:48:43 +0530

    Added the watcher service

---
+++ Day27/src/Services/EmployeePayrollWatcherService.java
@@ -0,0 +1,61 @@
+package  Services;
+import java.nio.file.*;
+
+public class EmployeePayrollWatcherService {
+
+    public static void main(String[] args)
+
+            throws Exception {
+
+        WatchService watchService =
+
+                FileSystems
+                        .getDefault()
+                        .newWatchService();
+
+        Path path =
+                Paths.get(
+                        "EmployeeData"
+                );
+
+        path.register(
+
+                watchService,
+
+                StandardWatchEventKinds
+                        .ENTRY_CREATE,
+
+                StandardWatchEventKinds
+                        .ENTRY_DELETE
+        );
+
+        System.out.println(
+                "Watching Directory..."
+        );
+
+        while(true) {
+
+            WatchKey key =
+                    watchService.take();
+
+            for(WatchEvent<?> event
+                    : key.pollEvents()) {
+
+                System.out.println(
+
+                        event.kind()
+
+                                +
+
+                                " : "
+
+                                +
+
+                                event.context()
+                );
+            }
+
+            key.reset();
+        }
+    }
+}
