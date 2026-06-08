276131846f4e66be89c55583fdcd5bd52adc6788
AbinayaSelvam <sabinaya352@gmail.com>
2026-06-08 16:53:51 +0530

    Merge main

--- Day27/src/Test/EmployeePayrollServiceTest.java
+++ Day27/src/Test/EmployeePayrollServiceTest.java
@@ -1,63 +1,101 @@
-//package Test;
-//import Entities.Employee;
-//import Services.EmployeeService;
-//import java.io.IOException;
-//import static org.junit.Assert.assertEquals;
-//
-//public class EmployeePayrollServiceTest {
-//
-//    @Test
-//    void given3Employees_WhenWrittenToFile_ShouldMatchEntries()
-//
-//            throws IOException {
-//
-//        EmployeeService service =
-//
-//                new EmployeeService();
-//
-//        service.addEmployee(
-//
-//                new Employee(
-//                        1,
-//                        "Abi",
-//                        50000
-//                )
-//        );
-//
-//        service.addEmployee(
-//
-//                new Employee(
-//                        2,
-//                        "John",
-//                        60000
-//                )
-//        );
-//
-//        service.addEmployee(
-//
-//                new Employee(
-//                        3,
-//                        "Sam",
-//                        70000
-//                )
-//        );
-//
-//        String fileName =
-//                "payroll.txt";
-//
-//        service.writeEmployeePayrollToFile(
-//                fileName
-//        );
-//
-//        long entries =
-//
-//                service.countEntries(
-//                        fileName
-//                );
-//
-//        assertEquals(
-//                3,
-//                entries
-//        );
-//    }
-//}
+package Test;
+import Entities.Employee;
+import Services.EmployeeService;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EmployeePayrollServiceTest {
+
+    @Test
+    void given3Employees_WhenWrittenToFile_ShouldMatchEntries()
+            throws IOException {
+
+        EmployeeService service = new EmployeeService();
+
+        service.addEmployee(
+                new Employee(
+                        1,
+                        "Abi",
+                        50000
+                )
+        );
+
+        service.addEmployee(
+                new Employee(
+                        2,
+                        "John",
+                        60000
+                )
+        );
+
+        service.addEmployee(
+                new Employee(
+                        3,
+                        "Sam",
+                        70000
+                )
+        );
+
+        String fileName = "payroll.txt";
+
+        service.writeEmployeePayrollToFile(
+                fileName
+        );
+
+        long entries =
+                service.countEntries(
+                        fileName
+                );
+
+        assertEquals(
+                3,
+                entries
+        );
+    }
+    @Test
+    void givenEmployeePayrollFile_WhenRead_ShouldReturnThreeEntries()
+            throws IOException {
+
+        EmployeeService service =
+                new EmployeeService();
+
+        service.addEmployee(
+                new Employee(
+                        1,
+                        "Abi",
+                        50000
+                )
+        );
+
+        service.addEmployee(
+                new Employee(
+                        2,
+                        "John",
+                        60000
+                )
+        );
+
+        service.addEmployee(
+                new Employee(
+                        3,
+                        "Sam",
+                        70000
+                )
+        );
+
+        String fileName =
+                "payroll.txt";
+
+        service.writeEmployeePayrollToFile(
+                fileName
+        );
+
+        assertEquals(
+                3,
+                service.readPayrollFile(
+                        fileName
+                ).size()
+        );
+    }
+}
