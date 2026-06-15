f1c64b14829a32c81b5171ef3a3c034de54106ca
AbinayaSelvam <sabinaya352@gmail.com>
2026-06-15 14:38:40 +0530

    Updated the getByName method

--- Day27/src/Entities/Employee.java
+++ Day27/src/Entities/Employee.java
@@ -1,6 +1,7 @@
 package Entities;
 
 public class Employee {
+<<<<<<< Updated upstream
     public int id;
     public String name;
     public double salary;
@@ -14,5 +15,29 @@ public class Employee {
     public String toString()
     {
         return id+" | "+name+" | "+salary;
+=======
+    private int id;
+    private String name;
+    private double salary;
+
+
+
+    public Employee(
+            int id,
+            String name,
+            double salary) {
+
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+>>>>>>> Stashed changes
     }
 }
